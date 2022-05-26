cfcbe28057b0f1f6642146d8089f8b037277fc8f
SrijaKotipalli <87261485+SrijaKotipalli@users.noreply.github.com>
2022-05-26 18:09:49 +0530

    Add files via upload
    while & for Loop simple programs

---
+++ loops.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 25 23:09:58 2022
+
+@author: Srija Anitha
+"""
+
+#looping structres
+#while Loop example
+#print n natural numbers
+n=int(input("enter n value"))
+i=0
+while(i<=n):
+    print(i)
+    i=i+1
+
+#for loop
+for j in range(10):
+    print ("j")
+    
+    
