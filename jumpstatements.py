eb3ca1d5d3934420a43f99741b95850c87e2f27b
SrijaKotipalli <87261485+SrijaKotipalli@users.noreply.github.com>
2022-05-26 18:48:32 +0530

    Add files via upload
    break & Continue programs

---
+++ jumpstatements.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 18:15:44 2022
+
+@author: Srija Anitha
+"""
+
+#unconditional jumpstatements
+#break
+i=1
+while(i<=10):
+    print (i)
+    if(i==5):
+        break
+    i=i+1
+print("loop terminated")
+
+#continue
+for j in range(1,10):
+    if(j==6):
+        continue
+    print(j)
+print("loop terminated")
